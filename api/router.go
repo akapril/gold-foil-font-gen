fec70da04805c8499f3ed4c1f9fd4d49e1583632
deathsw <deathswaltz@163.com>
2024-12-14 08:49:56 +0800

    vercel deploy

--- api/router.go
+++ api/router.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"gold-foil-font-gen/handler"
+	"net/http"
 )
 
 var Router *gin.Engine
@@ -14,4 +15,7 @@ func init() {
 	r.GET("/gold-foil-image", apiHandler.GoldFoilImage)
 	r.GET("/gold-foil-svg", apiHandler.GoldFoilSvg)
 	Router.GET("/img/gold-foil-image.png", apiHandler.GoldFoilImageFile)
+}
+func Listen(w http.ResponseWriter, r *http.Request) {
+	Router.ServeHTTP(w, r)
 }
